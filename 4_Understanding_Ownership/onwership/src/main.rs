1cb6ec30fc355c7e68f21a954a411d4dad91b9c8
Eduardo Sabino <eduardosabino@ymail.com>
2022-06-23 22:48:41 -0300

    Copy, Move e Clone

---
+++ 4_Understanding_Ownership/onwership/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    // let s = "Hello";
+    // let mut s2 = String::from("Hello s2");
+    // s2.push_str(", world");
+    // println!("{}", s2);
+
+    // let mut x = 5;
+    // let y = x;
+    // let z = y;
+    // x += 1;
+    // println!("{} {} {}", x, y, z);
+
+    let s = String::from("Hello");
+    let s1 = s.clone();
+    println!("{}", s)
+}
